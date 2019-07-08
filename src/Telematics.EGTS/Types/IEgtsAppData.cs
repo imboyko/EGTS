ef719792eedc70cc90370e3cfdac16846cc63a30
Ivan Boyko <i.boyko@1ckab.ru>
2019-07-08 21:36:41 +0300

    feat: добавлен интерфейс IEgtsAppData
    [#2]

---
+++ src/Telematics.EGTS/Types/IEgtsAppData.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Telematics.EGTS.Types
+{
+    interface IEgtsAppData
+    {
+        int Count { get; }
+
+        void Add(DataRecord item);
+        void Clear();
+        void Remove(DataRecord item);
+        void RemoveAt(int index);
+    }
+}
