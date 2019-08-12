b918fda7ed0ef713632e28ee73b3ee43048e6d14
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:42:16 +0300

    Добавлен класс EGTS_SR_AUTH_INFO
    #10

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_AUTH_INFO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись предназначена для передачи на авторизующую ТП аутентификационных данных АС (авторизуемой ТП) с использованием ранее переданных со стороны авторизующей ТП параметров для осуществления шифрования данных
+    /// </summary>
+    sealed class EGTS_SR_AUTH_INFO : IEgtsDataSubrecord
+    {
+        private string _UNM;
+        private string _UPSW;
+        private string _SS;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_AUTH_INFO;
+    }
+}
