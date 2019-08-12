42aa68dd945b5e5443a226a4f3828e826d8d79e9
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:40:09 +0300

    Добавлен класс EGTS_SR_AUTH_PARAMS
    #9

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_AUTH_PARAMS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись используется авторизующей ТП для передачи на АС данных о способе и параметрах шифрования, требуемого для дальнейшего взаимодействия
+    /// </summary>
+    sealed class EGTS_SR_AUTH_PARAMS : IEgtsDataSubrecord
+    {
+        private byte _Flags;
+        private ushort _PKL;
+        private byte[] _PBK;
+        private ushort _ISL;
+        private ushort _MSZ;
+        private string _SS;
+        private string _EXP;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_AUTH_PARAMS;
+    }
+}
