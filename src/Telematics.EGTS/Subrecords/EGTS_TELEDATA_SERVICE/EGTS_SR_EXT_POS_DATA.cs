bd3893ae1836c7b8cd87768a11bd9b5bb55503b7
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:48:55 +0300

    Добавлен класс EGTS_SR_EXT_POS_DATA
    #13

---
+++ src/Telematics.EGTS/Subrecords/EGTS_TELEDATA_SERVICE/EGTS_SR_EXT_POS_DATA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS.Subrecords.EGTS_TELEDATA_SERVICE
+{
+    /// <summary>
+    /// Подзапись используется АТ при передаче дополнительных данных определения местоположения
+    /// </summary>
+    sealed class EGTS_SR_EXT_POS_DATA : IEgtsDataSubrecord
+    {
+        private byte _Flags;
+        private ushort _VDOP;
+        private ushort _HDOP;
+        private ushort _PDOP;
+        private byte _SAT;
+        private ushort _NS;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_EXT_POS_DATA;
+    }
+}
