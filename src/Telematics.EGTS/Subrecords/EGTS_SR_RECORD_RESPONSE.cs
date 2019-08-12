25aebda62dcc013d3898f9de689a2465d33154e7
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:14:47 +0300

    Добавлен класс EGTS_SR_RECORD_RESPONSE
    #4

---
+++ src/Telematics.EGTS/Subrecords/EGTS_SR_RECORD_RESPONSE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись применяется для осуществления подтверждения процесса обработки записи Протокола Уровня Поддержки Услуг.
+    /// </summary>
+    public sealed class EGTS_SR_RECORD_RESPONSE : IEgtsDataSubrecord
+    {
+        private ushort _CRN;
+        private byte _RST;  // ProcessingCode
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_RECORD_RESPONSE;
+    }
+}
