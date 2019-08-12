ae8bf677b8af7f7bc25969c132aae12fa48d120a
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:44:33 +0300

    Добавлен класс EGTS_SR_SERVICE_INFO
    #11

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_SERVICE_INFO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Данный тип подзаписи используется для информирования принимающей стороны, АС или ТП, в зависимости от направления отправки, о поддерживаемых Сервисах, а также для запроса определённого набора требуемых Сервисов (от АС к ТП)
+    /// </summary>
+    sealed class EGTS_SR_SERVICE_INFO : IEgtsDataSubrecord
+    {
+        private byte _ST;
+        private byte _SSR;
+        private byte _SRVP;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_SERVICE_INFO;
+    }
+}
