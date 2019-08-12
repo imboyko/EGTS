4ed0cbcb2955e7e13ecd67179734d71fb8d6c09e
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:36:09 +0300

    Добавлен класс EGTS_SR_RESULT_CODE
    #8

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_RESULT_CODE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись применяется авторизующей ТП для информирования АС (авторизуемой ТП) о результатах процедуры аутентификации АС
+    /// </summary>
+    sealed class EGTS_SR_RESULT_CODE : IEgtsDataSubrecord
+    {
+        private byte _RCD;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_RESULT_CODE;
+    }
+}
