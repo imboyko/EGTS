cb6c2c379fcc8830dc5331ed24ef3d08100a4950
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:16:31 +0300

    Добавлен класс EGTS_SR_TERM_IDENTITY
    #3

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_TERM_IDENTITY.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись используется только АС при запросе авторизации на авторизующей ТП и содержит учётные данные АС
+    /// </summary>
+    sealed class EGTS_SR_TERM_IDENTITY : IEgtsDataSubrecord
+    {
+        private uint _TID;
+        private byte _Flags;
+        private ushort _HDID;
+        private string _IMEI;
+        private string _IMSI;
+        private string _LNGC;
+        private ushort _NID;
+        private ushort _BS;
+        private string _MSISDN;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_TERM_IDENTITY;
+    }
+}
