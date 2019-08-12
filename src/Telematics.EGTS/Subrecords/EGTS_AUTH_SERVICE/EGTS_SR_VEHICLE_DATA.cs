bb8982aa82684e7794645e31a01c297391db9329
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:13:28 +0300

    Добавлен класс EGTS_SR_VEHICLE_DATA
    #5

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_VEHICLE_DATA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись применяется АС для передачи на ТП информации о транспортном средстве.
+    /// </summary>
+    sealed class EGTS_SR_VEHICLE_DATA : IEgtsDataSubrecord
+    {
+        private string _VIN;
+        private uint _VHT;
+        private uint _VPST;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_VEHICLE_DATA;
+    }
+}
