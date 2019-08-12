de1df2d325259a749b42dd5379073d2848e9cbbd
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:31:25 +0300

    Добавлен класс EGTS_SR_DISPATCHER_IDENTITY
    #7

---
+++ src/Telematics.EGTS/Subrecords/EGTS_AUTH_SERVICE/EGTS_SR_DISPATCHER_IDENTITY.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telematics.EGTS
+{
+    /// <summary>
+    /// Подзапись используется только авторизуемой ТП при запросе авторизации на авторизующей ТП и содержит учётные данные авторизуемой АС
+    /// </summary>
+    sealed class EGTS_SR_DISPATCHER_IDENTITY : IEgtsDataSubrecord
+    {
+        private byte _DT;
+        private uint _DID;
+        private string _DSCR;
+
+        public EgtsSubrecordType Type => EgtsSubrecordType.EGTS_SR_DISPATCHER_IDENTITY;
+    }
+}
