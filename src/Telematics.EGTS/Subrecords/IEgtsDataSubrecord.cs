b6254c169dfcaadaca12e2b39f9b369effd54c5e
Ivan Boyko <i.boyko@1ckab.ru>
2019-08-12 23:55:17 +0300

    Документация

--- src/Telematics.EGTS/Subrecords/IEgtsDataSubrecord.cs
+++ src/Telematics.EGTS/Subrecords/IEgtsDataSubrecord.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace Telematics.EGTS
 {
+    /// <summary>Представляет подзапись данных уровня поддержки услуг</summary>
     public interface IEgtsDataSubrecord
     {
+        /// <summary>Gets the type.</summary>
+        /// <value>The type.</value>
+        /// TODO Edit XML Comment Template for Type
         EgtsSubrecordType Type { get;}
     }
 }
